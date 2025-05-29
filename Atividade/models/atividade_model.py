e017f45718b88cd477e9d13283d1baef1b1c69f8
Bruna <bruna.fsilva@aluno.faculdadeimpacta.com.br>
2025-05-28 22:38:49 -0300

    docker ok

--- Atividade/models/atividade_model.py
+++ Atividade/models/atividade_model.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import relationship
 class Atividade(db.Model):
     __tablename__ = 'atividades'
     id_atividade = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    id_disciplina = db.Column(db.Integer, nullable=False) # ID da disciplina, não o nome
+    id_disciplina = db.Column(db.Integer, nullable=False)
     enunciado = db.Column(db.Text, nullable=False)
 
     submissoes = relationship('Submissao', backref='atividade', lazy=True, cascade="all, delete-orphan")
@@ -16,11 +16,10 @@ class Submissao(db.Model):
     __tablename__ = 'submissoes'
     id_submissao = db.Column(db.Integer, primary_key=True, autoincrement=True)
     id_atividade = db.Column(db.Integer, db.ForeignKey('atividades.id_atividade'), nullable=False)
-    id_aluno = db.Column(db.Integer, nullable=False) # ID do aluno
-    resposta = db.Column(db.Text, nullable=False) # A resposta textual/arquivo do aluno
-    nota = db.Column(db.Integer, nullable=True) # Nota atribuída à submissão
+    id_aluno = db.Column(db.Integer, nullable=False) 
+    resposta = db.Column(db.Text, nullable=False) 
+    nota = db.Column(db.Integer, nullable=True) 
 
     def __repr__(self):
         return f"<Submissao {self.id_submissao} - Atv {self.id_atividade} - Aluno {self.id_aluno}>"
 
-# Não precisamos de AtividadeNaoEncontrada, pois os erros serão tratados com 404 e 500.
